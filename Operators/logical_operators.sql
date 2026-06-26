e085f6bb9eecb48ea5d1d475aba7058cc20b919e
SQL <vkspt22@gmail.com>
2026-06-26 16:58:29 +0530

    init

--- Operators/logical_operators.sql
+++ Operators/logical_operators.sql
@@ -9,3 +9,12 @@ select ename, deptno, sal from employee where deptno=20 and sal<3000;
 
 -- WAQTD name and salary of the employee if emp earns More than 1250 but less than 3000 .
 select ename, sal from employee where sal>1250 and sal<3000;
+
+-- WAQTD name and deptno of the employees if the works in dept 10 or 20
+select ename, deptno from employee where deptno = 10 or deptno= 20;
+
+-- WAQTD name and sal and deptno of the employees If emp gets more than 1250 but less than 4000 and works in dept 20 .
+select ename, sal,deptno from employee where sal>1250 and sal<4000 and deptno=20;
+
+-- Test change 
+-- Test change2
--- push.bat
+++ push.bat
@@ -1,6 +1,19 @@
 @echo off
+
+echo ==========================
+echo Git Status
+echo ==========================
+git status
+
+echo.
 git add .
-set /p msg=hey 
+
+set /p msg=Enter commit message:
+
 git commit -m "%msg%"
-git push
+
+git push origin main
+
+echo.
+echo Done!
 pause
