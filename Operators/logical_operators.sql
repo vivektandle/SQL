e6906e1662c01bbd68892077c83d275d51520c13
SQL <vkspt22@gmail.com>
2026-06-26 16:32:27 +0530

    init

--- Operators/logical_operators.sql
+++ Operators/logical_operators.sql
@@ -1,2 +1,5 @@
 -- WAQTD name and deptno along with job for the employee working in dept 10 .
 select ename, deptno , job from employee where deptno=10;
+
+-- WAQTD name and deptno along with job for the employee working as manager in dept 10 .
+select ename, deptno, job from employee where job="MANAGER" and deptno=10;
