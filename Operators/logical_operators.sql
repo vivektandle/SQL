a346d10574aed9d9de5937d69a863c76569feed1
SQL <vkspt22@gmail.com>
2026-06-26 16:38:47 +0530

    inital

--- Operators/logical_operators.sql
+++ Operators/logical_operators.sql
@@ -6,3 +6,6 @@ select ename, deptno, job from employee where job="MANAGER" and deptno=10;
 
 -- WAQTD name , deptno , salary of the employee working in dept 20 and earning less than 3000 .
 select ename, deptno, sal from employee where deptno=20 and sal<3000;
+
+-- WAQTD name and salary of the employee if emp earns More than 1250 but less than 3000 .
+select ename, sal from employee where sal>1250 and sal<3000;
