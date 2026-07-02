4f5d7553a573b87bdc7c3ccd768e2f6af7c12e67
SQL <vkspt22@gmail.com>
2026-07-02 17:42:49 +0530

    Updated SQL files

--- Operators/special_operators.sql
+++ Operators/special_operators.sql
@@ -1,2 +1,5 @@
 -- WAQTD name and deptno of the employees working in dept 10 or 30 .
 select ename, deptno from employee where deptno in(10,30);
+
+-- WAQTD name and job of the employee working as a clerk or manager Or salesman .
+select ename, job from employee where job in ('CLERK', 'MANAGER', 'SALESMAN');
