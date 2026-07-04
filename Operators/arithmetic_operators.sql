0723f0fab05beaf67ecc764bf253cf159f35c1bf
SQL <vkspt22@gmail.com>
2026-07-04 13:33:40 +0530

    Updated SQL Arithmetic Operator files

--- Operators/arithmetic_operators.sql
+++ Operators/arithmetic_operators.sql
@@ -12,3 +12,6 @@ select ename, sal, sal*0.20 as bonus_salary from employee;
 
 -- Display employee name and monthly salary if annual salary is divided equally into 12 months  
 select ename, (sal*12)/ 12 from employee;
+
+--  Display employee name and salary after a 10% hike.
+select ename, sal+ sal*0.10 as salary_hike from employee;
