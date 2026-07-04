0d7101b941409112d01497d380014856fb033d7c
SQL <vkspt22@gmail.com>
2026-07-04 13:23:11 +0530

    Updated SQL Special Operator files

--- Operators/arithmetic_operators.sql
+++ Operators/arithmetic_operators.sql
@@ -1,2 +1,8 @@
 -- Display employee name and annual salary.
 select ename, sal*12 as annual_salary from employee;
+
+--  Display employee name and salary after a ₹1000 increment.
+select ename, sal+1000 as new_salary from employee ;
+
+-- Display employee name and salary after a ₹500 deduction 
+select ename, sal-500 as reduced_Salary from employee;
