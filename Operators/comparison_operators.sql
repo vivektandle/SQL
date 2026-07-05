567eaad7e67be0461d1d8f795f6d335d06f93726
SQL <vkspt22@gmail.com>
2026-07-05 23:43:58 +0530

    Updated SQL COMPARISION OPERATOR file

--- Operators/comparison_operators.sql
+++ Operators/comparison_operators.sql
@@ -11,3 +11,6 @@ select * from employee where job='CLERK';
 -- WAQTD names and salaries of employees whose salary is equal to 3000.
 select ename, sal from employee where sal=3000;
 
+-- WAQTD details of employees who are not working in department 20.
+select * from employee where deptno != 20;
+
