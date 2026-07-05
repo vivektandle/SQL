5b42812ed07e2ee1c9cc54477b27c1b29ce67095
SQL <vkspt22@gmail.com>
2026-07-05 23:43:32 +0530

    Updated SQL COMPARISION OPERATOR file

--- Operators/comparison_operators.sql
+++ Operators/comparison_operators.sql
@@ -8,4 +8,6 @@ select ename from employee where deptno=10;
 -- WAQTD details of employees whose job is CLERK.
 select * from employee where job='CLERK';
 
+-- WAQTD names and salaries of employees whose salary is equal to 3000.
+select ename, sal from employee where sal=3000;
 
