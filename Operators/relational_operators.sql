4cf0fec0a88f5321c60c85ddc77a5f17d1ba9c47
SQL <vkspt22@gmail.com>
2026-07-06 01:19:32 +0530

    Updated SQL Relational Operator file

--- Operators/relational_operators.sql
+++ Operators/relational_operators.sql
@@ -2,3 +2,7 @@
 
 -- WAQTD names of employees whose salary is greater than 2000.
 select ename from employee where sal>2000;
+
+
+-- WAQTD details of employees who are working in a department number greater than 20.
+select * from employee where deptno>20;
