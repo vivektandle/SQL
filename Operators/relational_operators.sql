9aca6150b9c615b82906b9dd3d8c3207273cd8da
SQL <vkspt22@gmail.com>
2026-07-06 01:21:50 +0530

    Updated SQL Relational Operator file

--- Operators/relational_operators.sql
+++ Operators/relational_operators.sql
@@ -18,3 +18,30 @@ select * from employee where comm >= 500;
  
 -- WAQTD names of employees whose department number is less than or equal to 20.
 select ename from employee where deptno<=20;
+
+
+
+-- WAQTD employee names and jobs whose salary is greater than or equal to 3000.
+select ename , job from employee where sal >= 3000;
+
+-- WAQTD details of employees whose manager number is less than 7800.
+select * from employee where mgr<7800; 
+
+-- WAQTD names and salaries of employees whose salary is less than or equal to 1250.
+select ename , sal from employee where sal<=1250;
+
+-- WAQTD details of employees who are working in departments greater than or equal to 20.
+select * from employee where deptno>=20;
+
+-- WAQTD names, jobs, and salaries of employees whose salary is greater than 2500.
+select ename, job, sal from employee where sal>2500;
+
+
+-- WAQTD details of employees whose commission is greater than or equal to 500.
+select * from employee where comm >= 500;
+ 
+-- WAQTD names of employees whose department number is less than or equal to 20.
+select ename from employee where deptno<=20;
+
+-- WAQTD employee names and jobs whose salary is greater than or equal to 3000.
+select ename , job from employee where sal >= 3000;
