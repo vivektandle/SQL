928da4d00940eff6740478d7db13e275124dbb95
SQL <vkspt22@gmail.com>
2026-07-13 14:38:35 +0530

    Updated SQL DDL file

---
+++ SQL_Statements/2_DML/dml.sql
@@ -0,0 +1,5 @@
+-- 1. INSERT: 
+-- Syntax: insert into table_name values(v1, v2, v3, ....);
+
+-- 2: UPDATE:
+-- Syntax: update table_name set column_name = Value, column_name = Value, ....., [where statement];
